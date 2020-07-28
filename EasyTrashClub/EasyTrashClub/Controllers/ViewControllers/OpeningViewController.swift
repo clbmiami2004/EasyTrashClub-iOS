b64804cebbc7284d50b302a8481fc326bdf642ec
James McDougall <jameswiliammcdougall@gmail.com>
2020-07-28 01:08:28 -0500

    Initial Project Commit

---
+++ EasyTrashClub/EasyTrashClub/Controllers/ViewControllers/OpeningViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OpeningViewController.swift
+//  EasyTrashClub
+//
+//  Created by Lambda_School_loaner_226 on 7/28/20.
+//  Copyright © 2020 EasyTrashClub. All rights reserved.
+//
+
+import UIKit
+
+class OpeningViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
